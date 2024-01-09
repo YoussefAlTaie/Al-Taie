04ffae07aae1a9a714d67fb07bedd6464265a0ca
YoussefAlTaie <155756367+YoussefAlTaie@users.noreply.github.com>
2024-01-09 23:33:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 
+const Discord = require("discord.js-selfbot-v13");
+const { userAccount } = require("sphinx-run");
+
+const tokens = process.env["token"].split("**");
+
+for (const token of tokens) {
+  const client = new Discord.Client({
+    intents: [Discord.Intents.FLAGS.GUILDS]
+  });
+
+  client.on('ready', async () => {
+    console.log(`${client.user.username} is ready!`);
+  });
+
+  new userAccount(client, Discord).leveling({
+    channel: '1187808730440540294',
+    randomLetters: false,
+    time: 600000,
+    type: 'ar'
+  });
+
+  client.login(token)
